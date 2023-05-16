6b03c332e590cd0dc9d542a71c8abbd4ac05886c
JuliValencia <juliana.valencia1@gmail.com>
2023-05-16 16:35:10 +0200

    Created Dictionary component with search engine setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <a href="/" className="btn btn-primary shadow">
-        {" "}
-        Hello{" "}
-      </a>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          This project was coded by{" "}
+          <a
+            href="https://julianavalencia.net/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Juliana Valencia{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/JuliValencia/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
